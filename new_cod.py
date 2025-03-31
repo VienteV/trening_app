31eb4f9f79970ed2a4dcef9e13e49b3030f3a6fd
Maksim <maximovmd@yandex.ru>
2025-03-31 10:47:59 +0300

    function to class

--- new_cod.py
+++ new_cod.py
@@ -1,10 +1,18 @@
-def new_func():
-    a = 1
-    while True:
-        a += 1
-        yield a
+class New_class():
+    def __init__(self, a):
+        self.a = a
+        self.n = 0
+    def __iter__(self):
+        return self
 
-a = new_func()
+    def __next__(self):
+        b = self.a ** self.n
+        self.n += 1
+        return b
 
-for i in range(10):
-    print(next(a))
+n = New_class(2)
+
+i = 10
+while i > 0:
+    print(next(n))
+    i -= 1
